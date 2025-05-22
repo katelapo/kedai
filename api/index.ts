b4b0e96746eefe52b9c552505ca606c22b4581ad
ramadani <hubillahpunk@gmail.com>
2025-05-22 09:28:24 +0700

    binding database

--- api/index.ts
+++ api/index.ts
@@ -1,30 +1,17 @@
 interface Env {
     ASSET: Fetcher;
+    DB: D1Database;
 }
 
 export default {
-    fetch(request, env) {
+    async fetch(request, env) {
         const url = new URL(request.url);
 
         if (url.pathname.startsWith("/api/kedais")) {
-            return Response.json([
-                {
-                    id: 1,
-                    name: "Kedai A",
-                    location: "Location A",
-                    rating: 4.5,
-                    imageUrl: "https://example.com/kedai-a.jpg"
-                },
-                {
-                    id: 2,
-                    name: "Kedai B",
-                    location: "Location B",
-                    rating: 4.0,
-                    imageUrl: "https://example.com/kedai-b.jpg"
-                },
-            ]);
+            let result = await env.DB.prepare("SELECT * FROM kedais").all();
+            return Response.json(result);
         }
 
         return env.ASSET.fetch(request);
-    },
+    }
 }satisfies ExportedHandler<Env>;
