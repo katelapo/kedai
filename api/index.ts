65aa2683bd22977c44d7da75c799c81b207fe64c
ramadani <hubillahpunk@gmail.com>
2025-05-25 09:09:27 +0700

    .

--- api/index.ts
+++ api/index.ts
@@ -8,13 +8,13 @@ export default {
         const url = new URL(request.url);
 
         if (url.pathname.startsWith("/api/kedais")) {
-           if (request.method == "GET") {
+           if (request.method == 'GET') {
                 let result = await env.DB.prepare("SELECT * FROM kedai").all();
                 return Response.json(result.results);
-        }       else if (request.method == "POST") {
+        }       else if (request.method == 'POST') {
                 const newId = crypto.randomUUID()
                 const input = await request.json<any>()
-                const query = 'INSERT INTO kedais (id, name, address, time) VALUES ("${newId}", "${input.name}", "${input.address}", "${input.time}")';
+                const query = 'INSERT INTO kedais(id,name,address,time) values ("${newId}", "${input.name}", "${input.address}", ${input.time})';
                 const newKedai = await env.DB.exec(query);
                 return Response.json(newKedai);
         }
